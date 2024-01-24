ee7239fd3d5f094c31fc6278c138e70cc87d2db5
davi120ps <santos111@outlook.com>
2024-01-24 09:35:32 -0500

    nuevas cosas

--- index.js
+++ index.js
@@ -7,16 +7,42 @@ const express = require ("express");
 const app = express();
 
 app.get('/',function (req,res){
-    res.send('mi primera ruta funciona')
+    res.send('Ruta principal')
 });
 
-app.get('/alumnos',function(req,res){
+app.post('/servidor',function(req,res){
     res.json({
-        "nombre":"David Peña",
-        "carrera":"Software",
-        "edad": "19"
+        "servidor":"Activo"
     })
 });
+
+app.post('/alumno',function(req,res){
+    res.json(
+            {"nombre":"David","apellido":"Peña Santos","matricula": "19","carrera":"Software"}
+    )
+});
+
+app.post('/producto',function(req,res){
+    res.json(
+        [
+            {"producto":"shampoo","cantidad":"10","precio":"80.00"},
+            {"producto":"jabon","cantidad":"2","precio":"7.49"},
+            {"producto":"estropajo","cantidad":"5","precio":"21.99"}
+        ]        
+    )
+});
+
+app.post('/metodos',function(req,res){
+    res.json(
+        [
+            {"metodo":"get","descripcion":"Solicita una representacion de un recuerso especifico. Las peticiones que usan el metodo GET solo deben recuperar datos"},
+            {"metodo":"post","descripcion":"Se utiliza para enviar una entidad a un recuerso en especifico, causando a menudo un camnoi en el estado o efectos secundarios en el servidor"},
+            {"metodo":"patch","descripcion":"Es utilizado para aplicar modificaciones parcilaes a un recurso"},
+            {"metodo":"delete","descripcion":"Borra un recuerso en especifico"}
+        ]        
+    )
+});
+
 
 const port = 3000;
 app.listen(port,()=>{
